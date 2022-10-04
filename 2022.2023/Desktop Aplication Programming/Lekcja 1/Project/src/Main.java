0b04ce72eb178de7d23827e61f1b76b20f8c4c11
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-10-04 13:32:50 +0200

    Java lesson 1

--- 2022.2023/Desktop Aplication Programming/Lekcja 1/Project/src/Main.java
+++ 2022.2023/Desktop Aplication Programming/Lekcja 1/Project/src/Main.java
@@ -1,16 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main
          {
              private static void zrobOkno()
              {
                      JFrame okno = new JFrame("Aplikacja");
-
                      okno.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
-                     okno.setSize( 400, 200 );
+                     okno.setBackground( Color.BLUE );
+                     okno.setSize( 800, 200 );
                      okno.setLocation( 100, 50 );
                      okno.setVisible( true );
+
                      }
              public static void main( String[] args )
              {
