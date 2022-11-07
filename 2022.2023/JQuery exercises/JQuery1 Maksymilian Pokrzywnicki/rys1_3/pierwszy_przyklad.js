1fa6c490bc0017d54d8f01ac229c11a4e5180202
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-11-07 20:04:58 +0100

    Jquery1

---
+++ 2022.2023/JQuery exercises/JQuery1 Maksymilian Pokrzywnicki/rys1_3/pierwszy_przyklad.js
@@ -0,0 +1,17 @@
+$(function () {
+  $('#page').css('background-color', '#00FFFF');
+  $('h2').css('letter-spacing', '5px');
+  $('.link').css({
+      'color': 'black',
+      'text-decoration': 'none',
+  });
+  $('[href="#"]').attr('href', 'https://pl.wikipedia.org/wiki/J%C4%99zyk_programowania');
+});
+
+$(function () {
+  $('a').odd().css({
+    'color': 'white',
+    'text-decoration': 'none',
+    'background-color': 'black',
+  })
+})
