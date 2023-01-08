e26cdd5aafd2c641d738014a6cab136ab5c1b871
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-01-08 19:54:49 +0100

    Task 22 - Listing 2.22

---
+++ 2022.2023/Byszewski/Java - console apps/Listing 2.22/Listing222.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Listing222
+{
+    public static void main( String[] args )
+    {
+        String [] propozycje = { "Tak, zapisz", "Raczej nie", "Anuluj" };
+        String pytanie = "Czy zapisać zmiany na dysku?";
+        String tytul = "Zapisywanie dokumentu";
+        String komunikat;
+
+        int odpowiedz = JOptionPane.showOptionDialog( null,
+        pytanie, tytul, JOptionPane.YES_NO_CANCEL_OPTION,
+        JOptionPane.QUESTION_MESSAGE, null, propozycje, propozycje[1]);
+
+        switch ( odpowiedz )
+        {
+            case JOptionPane.YES_OPTION : komunikat = "TAK"; break;
+            case JOptionPane.NO_OPTION : komunikat = "NIE"; break;
+            case JOptionPane.CANCEL_OPTION : komunikat = "ANULUJ"; break;
+            default : komunikat = "???"; break;
+        }
+        System.out.println( "Odpowiedź: " + komunikat );
+    }
+} 
