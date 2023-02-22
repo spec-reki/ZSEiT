0f1be3f6deb5f3824bcee78da03658e7e491bf6b
spec-reki <79382698+spec-reki@users.noreply.github.com>
2023-02-22 12:00:49 +0100

    Footer, Header, Layout, Login

--- 2022.2023/p.Byszewski/ZAW/Zadanie 01.1/crud/src/pages/_document.tsx
+++ 2022.2023/p.Byszewski/ZAW/Zadanie 01.1/crud/src/pages/_document.tsx
@@ -2,11 +2,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet" />
+      
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <div id="portal"></div>
+        <div id="portal2"></div>
       </body>
     </Html>
   )
