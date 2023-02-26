df1c6008a241ec97906053965f710654254d211c
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-02-26 13:52:28 +0100

    Projesct setted up

---
+++ 2022.2023/p.Byszewski/ZAW/crudapp/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+   
+    </div>
+  )
+}
