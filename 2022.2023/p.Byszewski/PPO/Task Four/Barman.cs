5a460b35a91076f112cb53afea6341127e63ee40
spec-reki <79382698+spec-reki@users.noreply.github.com>
2023-03-22 11:15:30 +0100

    Task 4

---
+++ 2022.2023/p.Byszewski/PPO/Task Four/Barman.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Barman
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
