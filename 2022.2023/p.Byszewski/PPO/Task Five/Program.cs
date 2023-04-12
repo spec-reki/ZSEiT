9d9659e511fcfb904518f4f8f36e47daa4f094bd
spec-reki <79382698+spec-reki@users.noreply.github.com>
2023-04-12 12:17:25 +0200

    Task five

---
+++ 2022.2023/p.Byszewski/PPO/Task Five/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Cabrio
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
