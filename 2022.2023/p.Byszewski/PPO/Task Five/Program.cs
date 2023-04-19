c5e12989bc40eb718d80c10c1acfb6accb4071bb
spec-reki <79382698+spec-reki@users.noreply.github.com>
2023-04-19 11:57:58 +0200

    Task 05

--- 2022.2023/p.Byszewski/PPO/Task Five/Program.cs
+++ 2022.2023/p.Byszewski/PPO/Task Five/Program.cs
@@ -3,12 +3,76 @@
 namespace MyNamespace
 {
     class Cabrio
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+{
+    private string name;
+    private string brand;
+    private bool isMoving;
+    private bool isRoofOpen;
 
-            Console.ReadKey();
-        }
+    public Cabrio(string name, string brand)
+    {
+        this.name = name;
+        this.brand = brand;
+        this.isMoving = false;
+        this.isRoofOpen = false;
     }
+
+    public void StartMoving()
+    {
+        this.isMoving = true;
+    }
+
+    public void StopMoving()
+    {
+        this.isMoving = false;
+    }
+
+    public void OpenRoof()
+    {
+        if (this.isMoving)
+        {
+            Console.WriteLine("Błąd: Nie możesz otworzyć dachu podczas jazdy!");
+            return;
+        }
+        this.isRoofOpen = true;
+        Console.WriteLine("Dach jest otwarty.");
+    }
+
+    public void CloseRoof()
+    {
+        if (this.isMoving)
+        {
+            Console.WriteLine("Błąd: nie możesz otworzyć dachu podczas jazdy!");
+            return;
+        }
+        this.isRoofOpen = false;
+        Console.WriteLine("Dach jest zamknięty.");
+    }
+ static void Main(string[] args)
+    {
+        Cabrio cabrio = new Cabrio("Miata", "Mazda");
+
+        Console.WriteLine("Przykład 1:");
+        cabrio.OpenRoof();
+        cabrio.StartMoving();
+        Console.WriteLine("Auto się przemieszcza!");
+        cabrio.CloseRoof();
+        cabrio.StopMoving();
+        Console.WriteLine("Auto się zatrzymało!");
+        cabrio.CloseRoof();
+        
+        Console.WriteLine("\nPrzykład 2:");
+        cabrio.StartMoving();
+        Console.WriteLine("Auto się przemieszcza!");
+        cabrio.OpenRoof();
+        cabrio.StopMoving();
+        Console.WriteLine("Auto się zatrzymało!");
+        cabrio.OpenRoof();
+        
+
+        Console.ReadLine();
+}
+
+}
+
 }
